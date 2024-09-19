9b0f2758e2854b852036695cf9c61397a6e6306c
tohutohu <tohu.soy@gmail.com>
2024-09-19 11:01:06 +0900

    feat: ユーザー登録ができる

---
+++ webapp/go/models.go
@@ -0,0 +1,25 @@
+package main
+
+import "time"
+
+type User struct {
+	ID          string    `db:"id"`
+	Username    string    `db:"username"`
+	Firstname   string    `db:"firstname"`
+	Lastname    string    `db:"lastname"`
+	AccessToken string    `db:"access_token"`
+	CreatedAt   time.Time `db:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at"`
+}
+
+type Driver struct {
+	ID        string    `db:"id"`
+	Username  string    `db:"username"`
+	Firstname string    `db:"firstname"`
+	Lastname  string    `db:"lastname"`
+	CarModel  string    `db:"car_model"`
+	CarNo     string    `db:"car_no"`
+	IsActive  bool      `db:"is_active"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
