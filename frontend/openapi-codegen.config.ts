08e7bab3f0013c059b4baee8cff780ef144af0b0
imamiya-masaki <imamiyagit@gmail.com>
2024-10-01 16:35:48 +0900

    mv api-fetcher

--- frontend/openapi-codegen.config.ts
+++ frontend/openapi-codegen.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       relativePath: "../openapi/openapi.yaml",
       source: "file",
     },
-    outputDir: "./api-fetcher",
+    outputDir: "./app/api-fetcher",
     to: async (context) => {
       const filenamePrefix = "isucon";
       const { schemasFiles } = await generateSchemaTypes(context, {
