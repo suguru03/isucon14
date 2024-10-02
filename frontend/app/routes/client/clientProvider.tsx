c5e2fa00e353150dd82715be8fb5cd2d82842467
imamiya-masaki <imamiyagit@gmail.com>
2024-10-02 12:16:08 +0900

    userClient

--- frontend/app/routes/client/clientProvider.tsx
+++ frontend/app/routes/client/clientProvider.tsx
@@ -1,23 +1,28 @@
 import { ReactNode, createContext, useContext } from "react";
 
+export type AccessToken = string;
+
 /**
  * フロント側で利用するクライアント情報
  */
 type ClientInfo = {
   id: string,
-  access_token: string
+  name: string,
+  accessToken: AccessToken
 }
 
 const clientContext = createContext<ClientInfo>({
   id: "",
-  access_token: "",
+  name: "",
+  accessToken: "",
 })
 
-export const clientProvider = ({children, access_token}: { children: ReactNode, access_token:　string }) => {
+export const ClientProvider = ({children, accessToken}: { children: ReactNode, accessToken: string }) => {
   /**
    * openapi上にfetchするものがないので一旦仮置き
+   * 想定では、ここで通信を行い子供に流す。
    */
-  const fetchedValue = {id: "fetched", access_token}
+  const fetchedValue = {id: "fetched-id", name: "fetched-name", accessToken}
 
   return (
   <clientContext.Provider value={fetchedValue}>
@@ -25,4 +30,4 @@ export const clientProvider = ({children, access_token}: { children: ReactNode, 
   </clientContext.Provider>)
 }
 
-export const useUser = () => useContext(clientContext);
+export const useClient = () => useContext(clientContext);
