fba9bd8996533b3483c870693558861e112df0bc
imamiya-masaki <imamiyagit@gmail.com>
2024-10-07 18:26:20 +0900

    rootにQueryClientのprovider配置

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -6,8 +6,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  // QueryClientのインスタンスを作成
+  const queryClient = new QueryClient();
+
   return (
     <html lang="ja">
       <head>
@@ -17,7 +21,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
