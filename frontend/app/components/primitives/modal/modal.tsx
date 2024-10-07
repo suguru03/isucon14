f80e044e646d9a26d8d6bb6e48563a72a16b600f
munepom <munepom@users.noreply.github.com>
2024-10-07 17:30:31 +0900

    :recycle: useOnClickOutside へ切り出した

--- frontend/app/components/primitives/modal/modal.tsx
+++ frontend/app/components/primitives/modal/modal.tsx
@@ -6,6 +6,7 @@ import {
   useEffect,
   useImperativeHandle,
 } from "react";
+import { useOnClickOutside } from "~/components/hooks/useOnClickOutside";
 
 type ModalProps = PropsWithChildren<{
   onClose?: () => void;
@@ -14,28 +15,6 @@ type ModalProps = PropsWithChildren<{
 export const Modal = forwardRef<{ close: () => void }, ModalProps>(
   ({ children, onClose }, ref) => {
     const sheetRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-      const handleOutsideClick = (e: MouseEvent) => {
-        if (sheetRef.current && !sheetRef.current.contains(e.target as Node)) {
-          handleClose();
-        }
-      };
-
-      document.addEventListener("click", handleOutsideClick);
-
-      return () => {
-        document.removeEventListener("click", handleOutsideClick);
-      };
-    }, [onClose]);
-
-    useEffect(() => {
-      setTimeout(() => {
-        if (sheetRef.current) {
-          sheetRef.current.style.transform = `translateY(0)`;
-        }
-      }, 50); // アニメーション付きで描画するためのおまじない
-    }, []);
 
     const handleClose = () => {
       if (sheetRef.current) {
@@ -52,13 +31,24 @@ export const Modal = forwardRef<{ close: () => void }, ModalProps>(
       }
     };
 
+    useEffect(() => {
+      setTimeout(() => {
+        if (sheetRef.current) {
+          sheetRef.current.style.transform = `translateY(0)`;
+        }
+      }, 50); // アニメーション付きで描画するためのおまじない
+    }, []);
+
+    useOnClickOutside(sheetRef, handleClose);
+
     useImperativeHandle(ref, () => ({
       close: handleClose,
     }));
 
     return (
       <>
-        <div className="fixed inset-0 bg-black opacity-50 z-40"></div>{/* overlay */}
+        <div className="fixed inset-0 bg-black opacity-50 z-40"></div>
+        {/* overlay */}
         <div
           className={
             "fixed bottom-0 left-0 right-0 h-[90vh] bg-white border-t border-l border-r border-gray-300 rounded-t-3xl shadow-lg transition-transform duration-300 ease-in-out z-50"
