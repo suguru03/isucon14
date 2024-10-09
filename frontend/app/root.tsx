c171b2dc028949e3d0bc6e1c53851e78be64a1f4
Masanari Hamada <narirou.github@gmail.com>
2024-10-10 00:37:32 +0900

    feat: map component base

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -19,7 +19,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="overscroll-none">
         <QueryClientProvider client={queryClient}>
           {children}
         </QueryClientProvider>
