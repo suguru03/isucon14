6b2d77171aa841a663d23d3c4d34154c2d9c3f52
munepom <munepom@users.noreply.github.com>
2024-10-10 11:04:59 +0900

    :+1: added interaction.

--- frontend/app/routes/client._index/requestComponent/reception.tsx
+++ frontend/app/routes/client._index/requestComponent/reception.tsx
@@ -1,9 +1,21 @@
 import { Button } from "~/components/primitives/button/button";
 import type { RequestProps } from "./type";
+import { useState } from "react";
+import { ReceptionMapModal } from "./receptionMapModal";
 
 export const Reception = ({
   status,
 }: RequestProps<"IDLE" | "MATCHING" | "DISPATCHING">) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const onCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       {status === "IDLE" ? (
@@ -12,14 +24,16 @@ export const Reception = ({
         <div>配車しています</div>
       )}
       <div className="px-4 py-16 block justify-center border-t">
-        <Button onClick={() => {}}>from</Button>
-        <Button onClick={() => {}}>to</Button>
+        <Button onClick={handleOpenModal}>from</Button>
+        <Button onClick={handleOpenModal}>to</Button>
         {status === "IDLE" ? (
           <Button onClick={() => {}}>配車</Button>
         ) : (
           <Button onClick={() => {}}>配車をキャンセルする</Button>
         )}
       </div>
+
+      {isModalOpen && <ReceptionMapModal onClose={onCloseModal} />}
     </>
   );
 };
