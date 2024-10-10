9f64e02d96ca92390a6286628f3a57c158f21e34
imamiya-masaki <imamiyagit@gmail.com>
2024-10-10 14:48:26 +0900

    clientregister fmt

--- frontend/app/routes/client_.register/route.tsx
+++ frontend/app/routes/client_.register/route.tsx
@@ -1,7 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { redirect, ClientActionFunctionArgs} from "@remix-run/react";
+import { redirect, ClientActionFunctionArgs , Form } from "@remix-run/react";
 import { fetchAppPostRegister } from "~/apiClient/apiComponents";
-import { Form } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,7 +10,6 @@ export const meta: MetaFunction = () => {
 };
 
 export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
-  
   const formData = await request.formData();
   const data = await fetchAppPostRegister({
     body: {
@@ -19,46 +17,54 @@ export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
       username: String(formData.get("username")) ?? "",
       firstname: String(formData.get("firstname")) ?? "",
       lastname: String(formData.get("lastname")) ?? "",
-    }
-  })
-  return redirect(`/client?access_token=${data.access_token}&user_id=${data.id}`)
+    },
+  });
+  return redirect(
+    `/client?access_token=${data.access_token}&user_id=${data.id}`,
+  );
 };
 
 export default function ClientRegister() {
-
   return (
     <>
-    <Form  className="mx-auto p-6 bg-white rounded-lg shadow-md space-y-4 flex flex-col" method="POST">
-      <label htmlFor="username">Username:</label>
-      <input
-      type="text"
-      id="username"
-      name="username" 
-      className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-      required/>
-      <label htmlFor="firstname">Firstname:</label>
-      <input
-      type="text"
-      id="firstname"
-      name="firstname"
-      className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-      required />
-      <label htmlFor="lastname">Lastname:</label>
-      <input
-      type="text"
-      id="lastname"
-      name="lastname"
-      className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-      required />
-      <label htmlFor="date_of_birth">dateOfBirth:</label>
-      <input
-      type="text"
-      id="date_of_birth"
-      name="date_of_birth"
-      className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-      required />
-      <button type="submit">登録</button>
-    </Form>
+      <Form
+        className="mx-auto p-6 bg-white rounded-lg shadow-md space-y-4 flex flex-col"
+        method="POST"
+      >
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+        <label htmlFor="firstname">Firstname:</label>
+        <input
+          type="text"
+          id="firstname"
+          name="firstname"
+          className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+        <label htmlFor="lastname">Lastname:</label>
+        <input
+          type="text"
+          id="lastname"
+          name="lastname"
+          className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+        <label htmlFor="date_of_birth">dateOfBirth:</label>
+        <input
+          type="text"
+          id="date_of_birth"
+          name="date_of_birth"
+          className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+        <button type="submit">登録</button>
+      </Form>
     </>
   );
 }
