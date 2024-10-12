d2270bae89df0e5db35b8469a573389005324e65
Hibiki Seki <hibiki0719euph@gmail.com>
2024-10-12 10:23:43 +0900

    Update entrypoint.sh

--- bench/entrypoint.sh
+++ bench/entrypoint.sh
@@ -5,7 +5,7 @@ ZONE_ID=$(aws ec2 describe-availability-zones |
 	jq -r ".AvailabilityZones[] | select(.ZoneName==\"${ZONE_NAME}\").ZoneId")
 export ISUXPORTAL_SUPERVISOR_INSTANCE_NAME="${ZONE_ID}"
 
-PUBLIC_IP=$($ECS_CONTAINER_METADATA_URI_V4 | jq -r '.Networks[0].IPv4Addresses[0]')
+PUBLIC_IP=$(curl -s $ECS_CONTAINER_METADATA_URI_V4 | jq -r '.Networks[0].IPv4Addresses[0]')
 export ISUXBENCH_PUBLIC_IP="${PUBLIC_IP}"
 
 exec "$@"
