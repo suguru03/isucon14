5894e908cb6d475a0788d526ef73c7b4abf245a2
wtks <30363887+wtks@users.noreply.github.com>
2024-10-15 07:57:31 +0900

    clientの実装をworldのentityが持つように

--- bench/benchmarker/world/world.go
+++ bench/benchmarker/world/world.go
@@ -37,6 +37,8 @@ type World struct {
 	RequestDB *RequestDB
 	// PaymentDB 支払い結果DB
 	PaymentDB *PaymentDB
+	// Client webappへのクライアント
+	Client WorldClient
 	// RootRand ルートの乱数生成器
 	RootRand *rand.Rand
 	// CompletedRequestChan 完了したリクエストのチャンネル
@@ -55,7 +57,7 @@ type World struct {
 	TimeoutTickCount int
 }
 
-func NewWorld(tickTimeout time.Duration, completedRequestChan chan *Request) *World {
+func NewWorld(tickTimeout time.Duration, completedRequestChan chan *Request, client WorldClient) *World {
 	return &World{
 		Regions: []*Region{
 			NewRegion("A", 0, 0, 100, 100),
@@ -66,6 +68,7 @@ func NewWorld(tickTimeout time.Duration, completedRequestChan chan *Request) *Wo
 		ChairDB:              NewGenericDB[ChairID, *Chair](),
 		RequestDB:            NewRequestDB(),
 		PaymentDB:            NewPaymentDB(),
+		Client:               client,
 		RootRand:             random.NewLockedRand(rand.NewPCG(0, 0)),
 		CompletedRequestChan: completedRequestChan,
 		tickTimeout:          tickTimeout,
@@ -185,7 +188,7 @@ func (w *World) CreateUser(ctx *Context, args *CreateUserArgs) (*User, error) {
 		DateOfBirth: random.GenerateDateOfBirth(),
 	}
 
-	res, err := ctx.client.RegisterUser(ctx, &RegisterUserRequest{
+	res, err := w.Client.RegisterUser(ctx, &RegisterUserRequest{
 		UserName:    registeredData.UserName,
 		FirstName:   registeredData.FirstName,
 		LastName:    registeredData.LastName,
@@ -202,6 +205,7 @@ func (w *World) CreateUser(ctx *Context, args *CreateUserArgs) (*User, error) {
 		RegisteredData:    registeredData,
 		AccessToken:       res.AccessToken,
 		PaymentToken:      random.GeneratePaymentToken(),
+		Client:            res.Client,
 		Rand:              random.CreateChildRand(w.RootRand),
 		notificationQueue: make(chan NotificationEvent, 500),
 	}
@@ -223,7 +227,7 @@ func (w *World) CreateProvider(ctx *Context, args *CreateProviderArgs) (*Provide
 		Name: random.GenerateProviderName(),
 	}
 
-	res, err := ctx.client.RegisterProvider(ctx, &RegisterProviderRequest{
+	res, err := w.Client.RegisterProvider(ctx, &RegisterProviderRequest{
 		Name: registeredData.Name,
 	})
 	if err != nil {
@@ -236,6 +240,7 @@ func (w *World) CreateProvider(ctx *Context, args *CreateProviderArgs) (*Provide
 		ChairDB:        concurrent.NewSimpleMap[ChairID, *Chair](),
 		RegisteredData: registeredData,
 		AccessToken:    res.AccessToken,
+		Client:         res.Client,
 		Rand:           random.CreateChildRand(w.RootRand),
 	}
 	p.tickDone.Store(true)
@@ -259,7 +264,7 @@ func (w *World) CreateChair(ctx *Context, args *CreateChairArgs) (*Chair, error)
 		Model: random.GenerateChairModel(),
 	}
 
-	res, err := ctx.client.RegisterChair(ctx, args.Provider, &RegisterChairRequest{
+	res, err := args.Provider.Client.RegisterChair(ctx, args.Provider, &RegisterChairRequest{
 		Name:  registeredData.Name,
 		Model: registeredData.Model,
 	})
@@ -277,6 +282,7 @@ func (w *World) CreateChair(ctx *Context, args *CreateChairArgs) (*Chair, error)
 		WorkTime:          args.WorkTime,
 		RegisteredData:    registeredData,
 		AccessToken:       res.AccessToken,
+		Client:            res.Client,
 		Rand:              random.CreateChildRand(args.Provider.Rand),
 		notificationQueue: make(chan NotificationEvent, 500),
 	}
