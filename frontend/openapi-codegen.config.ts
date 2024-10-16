6205f7c4d47e18f4e9e88037cbe7ecfdef66e36a
imamiya-masaki <imamiyagit@gmail.com>
2024-10-16 11:44:20 +0900

    PROXY_URLをやめて、環境変数を扱えるようにする

--- frontend/openapi-codegen.config.ts
+++ frontend/openapi-codegen.config.ts
@@ -3,10 +3,10 @@ import {
   generateReactQueryComponents,
 } from "@openapi-codegen/typescript";
 import { defineConfig } from "@openapi-codegen/cli";
-import { writeFileSync } from "fs";
+import { writeFile, readdir, readFile } from "fs/promises";
+import {join as pathJoin} from "path";
 
 const outputDir = "./app/apiClient";
-
 export default defineConfig({
   isucon: {
     from: {
@@ -15,23 +15,10 @@ export default defineConfig({
     },
     outputDir,
     to: async (context) => {
-      const proxyURL = process.env.PROXY_URL;
-      if (proxyURL) {
-        console.log(`proxyURL: ${proxyURL}`);
-        const contextServers = context.openAPIDocument.servers;
-        context.openAPIDocument.servers = contextServers?.map(
-          (serverObject) => {
-            return {
-              ...serverObject,
-              url: proxyURL,
-            };
-          },
-        );
-      }
-
-      const targetBaseCandidateURLs = context.openAPIDocument.servers?.map(
-        (server) => server.url,
-      );
+      /**
+       * openapi.yamlに定義済みのurl配列
+       */
+        const targetBaseCandidateURLs = context.openAPIDocument.servers?.map((server) => server.url);
       if (
         targetBaseCandidateURLs === undefined ||
         targetBaseCandidateURLs.length === 0
@@ -41,9 +28,24 @@ export default defineConfig({
       if (targetBaseCandidateURLs.length > 1) {
         throw Error("he servers.url must have only one entry.");
       }
+
       const targetBaseURL = targetBaseCandidateURLs[0];
 
       const filenamePrefix = "API";
+      const placeholderTextForAPIURL = "API_BASE_URL_A9fXkLz8YmNp";
+      const alternativeAPIBaseURL = `process.env.API_BASE_URL || "${targetBaseURL}"`
+      const contextServers = context.openAPIDocument.servers;
+      /**
+       * 後で、任意のコードに置き換えるためにAPIのbaseURLをユニーク文字列に置き換える
+       */
+      context.openAPIDocument.servers = contextServers?.map(
+        (serverObject) => {
+          return {
+            ...serverObject,
+            url: placeholderTextForAPIURL,
+          };
+        },
+      );
       const { schemasFiles } = await generateSchemaTypes(context, {
         filenamePrefix,
       });
@@ -51,10 +53,44 @@ export default defineConfig({
         filenamePrefix,
         schemasFiles,
       });
-      writeFileSync(
+      await rewriteFileInTargetDir(outputDir, (content) => {
+        return content.replace(`"${placeholderTextForAPIURL}"`, alternativeAPIBaseURL);
+      })
+      
+      /**
+       * SSE通信などでは、自動生成のfetcherを利用しないため
+       */
+      await writeFile(
         `${outputDir}/${filenamePrefix}BaseURL.ts`,
-        `export const apiBaseURL = "${targetBaseURL}";\n`,
+        `export const apiBaseURL = ${alternativeAPIBaseURL};\n`,
       );
     },
   },
 });
+
+
+/**
+ * 指定されたディレクトリ配下のファイルコンテンツをrewriteFnで置き換える
+ */
+async function rewriteFileInTargetDir(
+  dirPath: string,
+  rewriteFn: (content: string) => string
+): Promise<void> {
+  try {
+    const files = await readdir(dirPath, { withFileTypes: true });
+    for (const file of files) {
+      const filePath = pathJoin(dirPath, file.name);
+      if (file.isDirectory()) {
+        await rewriteFileInTargetDir(filePath, rewriteFn);
+        continue;
+      }
+      if (file.isFile()) {
+        const data = await readFile(filePath, 'utf8');
+        const rewrittenContent = rewriteFn(data);
+        await writeFile(filePath, rewrittenContent);
+        }
+      }
+    } catch (err) {
+    console.error(`CONSOLE ERROR: ${err}`);
+  }
+}
