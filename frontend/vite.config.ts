0df1b3405cad4ffe3eacfb97bd3c18aedef293d6
imamiya-masaki <imamiyagit@gmail.com>
2024-10-21 18:30:45 +0900

    デフォルトを本番側に寄せる

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -123,7 +123,7 @@ export const config = {
     customConsolePlugin,
   ],
   define: {
-    [alternativeURLExpression]: `"${process.env["API_BASE_URL"] ?? "http://localhost:8080"}"`,
+    [alternativeURLExpression]: `"${process.env["API_BASE_URL"] ?? "/"}"`,
   },
   server: {
     proxy: {
