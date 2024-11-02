b83389865ad8b475bd2dc84f1d968ef8ea6e31c7
ryoha000 <the7uya@gmail.com>
2024-11-03 01:19:49 +0900

    feat: 🎸 [bench] payment が request に対して invalid かを判定するように

--- bench/benchmarker/world/payment.go
+++ bench/benchmarker/world/payment.go
@@ -6,26 +6,63 @@ import (
 	"github.com/samber/lo"
 )
 
+type invalidPaymentReason string
+
+const (
+	// 既に支払い済み
+	invalidPaymentReasonAlreadyPaid invalidPaymentReason = "already_paid"
+	// Amount が異なる
+	invalidPaymentReasonInvalidAmount invalidPaymentReason = "invalid_amount"
+	// 進行中のリクエストがない
+	invalidPaymentReasonNoRequest invalidPaymentReason = "no_request"
+)
+
+type invalidPayment struct {
+	Payment *payment.Payment
+	Request *Request
+	Reason  invalidPaymentReason
+}
+
 type PaymentDB struct {
 	PaymentTokens     *concurrent.SimpleMap[string, *User]
 	CommittedPayments *concurrent.SimpleSlice[*payment.Payment]
+	// TODO: このフィールドに入れるんじゃなくてdeductionとかにまとめる
+	invalidPayments    *concurrent.SimpleSlice[*invalidPayment]
+	alreadyPaidRequest *concurrent.SimpleMap[*Request, struct{}]
 }
 
 func NewPaymentDB() *PaymentDB {
 	return &PaymentDB{
-		PaymentTokens:     concurrent.NewSimpleMap[string, *User](),
-		CommittedPayments: concurrent.NewSimpleSlice[*payment.Payment](),
+		PaymentTokens:      concurrent.NewSimpleMap[string, *User](),
+		CommittedPayments:  concurrent.NewSimpleSlice[*payment.Payment](),
+		invalidPayments:    concurrent.NewSimpleSlice[*invalidPayment](),
+		alreadyPaidRequest: concurrent.NewSimpleMap[*Request, struct{}](),
 	}
 }
 
 func (db *PaymentDB) Verify(p *payment.Payment) payment.Status {
-	_, ok := db.PaymentTokens.Get(p.Token)
+	user, ok := db.PaymentTokens.Get(p.Token)
 	if !ok {
 		return payment.StatusInvalidToken
 	}
 	if p.Amount <= 0 && p.Amount > 1_000_000 {
 		return payment.StatusInvalidAmount
 	}
+
+	// 支払いがリクエストに対して valid かどうかを確認するが、
+	// Payment Server 自体はリクエストに対して valid かどうかに関わらず決済を行う
+	// このため、invalid だった場合も status には反映しないが invalidPayments に記録する
+	req := user.Request
+	if req == nil {
+		db.invalidPayments.Append(&invalidPayment{Payment: p, Reason: invalidPaymentReasonNoRequest})
+	}
+	if _, alreadyPaid := db.alreadyPaidRequest.Get(req); alreadyPaid {
+		db.invalidPayments.Append(&invalidPayment{Payment: p, Request: req, Reason: invalidPaymentReasonAlreadyPaid})
+	}
+	if p.Amount != req.Fare() {
+		db.invalidPayments.Append(&invalidPayment{Payment: p, Request: req, Reason: invalidPaymentReasonInvalidAmount})
+	}
+
 	db.CommittedPayments.Append(p)
 	return payment.StatusSuccess
 }
