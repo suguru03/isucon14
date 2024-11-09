a2874380dcd74f7d404ab324677c3fde680bfd88
AnaTofuZ <anatofuz@gmail.com>
2024-11-09 11:46:26 +0900

    app_post_payment_methodsに必要そうな一式を追加

---
+++ webapp/perl/lib/Isuride/Handler/App.pm
@@ -0,0 +1,30 @@
+package Isuride::Handler::App;
+use v5.40;
+use utf8;
+
+use HTTP::Status qw(:constants);
+use Types::Standard -types;
+
+use Isuride::Util qw(check_params);
+
+use constant AppPaymentMethodsRequest => Dict [ token => Str, ];
+
+sub app_post_payment_methods ( $self, $c ) {
+    my $params = $c->req->json_parameters;
+    unless ( check_params( $params, AppPaymentMethodsRequest ) ) {
+        return $c->halt( HTTP_BAD_REQUEST,
+            'failed to decode the request body as json' );
+    }
+
+    if ( $params->{token} eq '' ) {
+        return $c->halt( HTTP_BAD_REQUEST, 'token is required but was empt' );
+    }
+
+    my $user = $c->stash->{user};
+
+    $c->dbh->query(
+        q{INSERT INTO payment_methods (user_id, token) VALUES (?, ?)},
+        $user->{id}, $params->{token} );
+
+    $c->halt_no_content(HTTP_OK);
+}
