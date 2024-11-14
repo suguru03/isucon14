6bddc3facdac1995233a62aa324aa57e220c7bca
popunbom <28749104+popunbom@users.noreply.github.com>
2024-11-14 21:40:29 +0900

    bloat

--- frontend/app/contexts/simulator-context.tsx
+++ frontend/app/contexts/simulator-context.tsx
@@ -1,67 +1,23 @@
-import { ReactNode, createContext } from "react";
-import type { Coordinate } from "~/apiClient/apiSchemas";
+import { createContext, ReactNode, useContext } from "react";
 
-type SimulatorChair = {
-  name: string;
-  model: string;
-  chair_token: string;
-  coordinate: Coordinate;
-};
+type Data = object; //TODO: define schema
 
-type ClientProviderContextType = {
-  chairs: SimulatorChair[];
-};
+const SimulatorContext = createContext<Data>({});
 
-const ClientProviderContext = createContext<Partial<ClientProviderContextType>>(
-  {},
-);
-
-export const SimulatorProvider = ({
-  children,
-}: {
-  children: ReactNode;
-  providerId: string;
-}) => {
+export function SimulatorContextProvider(
+  {
+    children
+  } : {
+    children: ReactNode
+  }
+) {
+  const data: Data = {}; //TODO: fetch via API
+  
   return (
-    <ClientProviderContext.Provider value={{}}>
+    <SimulatorContext.Provider value={data}>
       {children}
-    </ClientProviderContext.Provider>
-  );
-};
+    </SimulatorContext.Provider>
+  )
+}
 
-export const useSimulatorContext = () => {
-  // TODO: 動的に作成できるようにする
-  const testSimulatorContext = {
-    chairs: [
-      {
-        chair_token: "token1",
-        coordinate: {
-          latitude: 100,
-          longitude: 100,
-        },
-        model: "testModel1",
-        name: "testName1",
-      },
-      {
-        chair_token: "token2",
-        coordinate: {
-          latitude: 200,
-          longitude: 200,
-        },
-        model: "testModel2",
-        name: "testName2",
-      },
-      {
-        chair_token: "token3",
-        coordinate: {
-          latitude: 300,
-          longitude: 300,
-        },
-        model: "testModel3",
-        name: "testName3",
-      },
-    ],
-  } satisfies ClientProviderContextType;
-
-  return testSimulatorContext;
-};
+export const useSimulatorContext = () => useContext(SimulatorContext);
