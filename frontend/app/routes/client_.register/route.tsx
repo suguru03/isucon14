56ec37d8617ab66d2128d95b0ab153647f9e8fa5
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2024-11-21 20:39:10 +0900

    決済トークン登録を別ページにする

--- frontend/app/routes/client_.register/route.tsx
+++ frontend/app/routes/client_.register/route.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
-import ClientLoginForm from "./form/login";
-import PaymentTokenRegisterForm from "./form/payment-register";
+import { ClientActionFunctionArgs, Form, redirect } from "@remix-run/react";
+import { fetchAppPostUsers } from "~/apiClient/apiComponents";
+import { Button } from "~/components/primitives/button/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,11 +10,59 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
+  const formData = await request.formData();
+  const data = await fetchAppPostUsers({
+    body: {
+      date_of_birth: String(formData.get("date_of_birth")),
+      username: String(formData.get("username")),
+      firstname: String(formData.get("firstname")),
+      lastname: String(formData.get("lastname")),
+    },
+  });
+  return redirect(`/client?id=${data.id}`);
+};
+
 export default function ClientRegister() {
   return (
     <>
-      <ClientLoginForm />
-      <PaymentTokenRegisterForm />
+      <Form className="p-4 flex flex-col gap-4" method="POST">
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+          <label htmlFor="firstname">Firstname:</label>
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+          <label htmlFor="lastname">Lastname:</label>
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+          <label htmlFor="date_of_birth">dateOfBirth:</label>
+          <input
+            type="text"
+            id="date_of_birth"
+            name="date_of_birth"
+            className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <Button type="submit">登録</Button>
+      </Form>
     </>
   );
 }
