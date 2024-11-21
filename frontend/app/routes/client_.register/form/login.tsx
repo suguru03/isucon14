9b350578e3a2121d3829644e8587c8d91ed67afe
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2024-11-21 19:03:31 +0900

    決済トークンの登録フォームを追加

---
+++ frontend/app/routes/client_.register/form/login.tsx
@@ -0,0 +1,69 @@
+import { useNavigate } from "@remix-run/react";
+import { useState } from "react";
+import { fetchAppPostUsers } from "~/apiClient/apiComponents";
+import { Button } from "~/components/primitives/button/button";
+
+export default function ClientLoginForm() {
+  const [username, setUsername] = useState<string>("");
+  const [firstname, setFirstname] = useState<string>("");
+  const [lastname, setLastname] = useState<string>("");
+  const [dateOfBirth, setDateOfBirth] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    const data = await fetchAppPostUsers({
+      body: {
+        date_of_birth: dateOfBirth,
+        username,
+        firstname,
+        lastname,
+      },
+    });
+
+    return navigate(`/client?id=${data.id}`);
+  };
+
+  return (
+    <div className="p-4 flex flex-col gap-4">
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="firstname">Firstname:</label>
+        <input
+          type="text"
+          id="firstname"
+          name="firstname"
+          className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <label htmlFor="lastname">Lastname:</label>
+        <input
+          type="text"
+          id="lastname"
+          name="lastname"
+          className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+          onChange={(e) => setLastname(e.target.value)}
+        />
+        <label htmlFor="date_of_birth">dateOfBirth:</label>
+        <input
+          type="text"
+          id="date_of_birth"
+          name="date_of_birth"
+          className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+          onChange={(e) => setDateOfBirth(e.target.value)}
+        />
+      </div>
+      <Button onClick={handleSubmit}>登録</Button>
+    </div>
+  );
+}
--- frontend/app/routes/client_.register/route.tsx
+++ frontend/app/routes/client_.register/route.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { ClientActionFunctionArgs, Form, redirect } from "@remix-run/react";
-import { fetchAppPostUsers } from "~/apiClient/apiComponents";
+import ClientLoginForm from "./form/login";
+import PaymentTokenRegisterForm from "./form/payment-register";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,62 +9,11 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
-  const formData = await request.formData();
-  const data = await fetchAppPostUsers({
-    body: {
-      date_of_birth: String(formData.get("date_of_birth")),
-      username: String(formData.get("username")),
-      firstname: String(formData.get("firstname")),
-      lastname: String(formData.get("lastname")),
-    },
-  });
-  return redirect(`/client?id=${data.id}`);
-};
-
 export default function ClientRegister() {
   return (
     <>
-      <Form
-        className="w-full h-full p-4 bg-white rounded-lg shadow-md flex flex-col gap-4"
-        method="POST"
-      >
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-          <label htmlFor="firstname">Firstname:</label>
-          <input
-            type="text"
-            id="firstname"
-            name="firstname"
-            className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-          <label htmlFor="lastname">Lastname:</label>
-          <input
-            type="text"
-            id="lastname"
-            name="lastname"
-            className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-          <label htmlFor="date_of_birth">dateOfBirth:</label>
-          <input
-            type="text"
-            id="date_of_birth"
-            name="date_of_birth"
-            className="mt-1 p-2 w-full border border-neutral-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-        <button type="submit">登録</button>
-      </Form>
+      <ClientLoginForm />
+      <PaymentTokenRegisterForm />
     </>
   );
 }
