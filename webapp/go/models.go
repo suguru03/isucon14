bc995e7497120b66e76d8008b91b6e84b48904c1
ryoha000 <the7uya@gmail.com>
2024-11-24 15:52:02 +0900

    fix: 🐛 ステータスの変更を絶対に通知するように

--- webapp/go/models.go
+++ webapp/go/models.go
@@ -61,10 +61,12 @@ type Ride struct {
 }
 
 type RideStatus struct {
-	ID        string    `db:"id"`
-	RideID    string    `db:"ride_id"`
-	Status    string    `db:"status"`
-	CreatedAt time.Time `db:"created_at"`
+	ID          string     `db:"id"`
+	RideID      string     `db:"ride_id"`
+	Status      string     `db:"status"`
+	CreatedAt   time.Time  `db:"created_at"`
+	AppSentAt   *time.Time `db:"app_sent_at"`
+	ChairSentAt *time.Time `db:"chair_sent_at"`
 }
 
 type Owner struct {
