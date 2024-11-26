ba7de3817086603fdeb64b5a13a5abe2cd5469d1
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2024-11-26 12:24:31 +0900

    iframeのtittle追加

--- frontend/app/routes/simulator._index/route.tsx
+++ frontend/app/routes/simulator._index/route.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <div className="h-screen min-h-[1000px] min-w-[1200px] flex justify-center items-center gap-32">
       <SmartPhone>
-        <iframe src="/client" className="w-full h-full" />
+        <iframe title="ユーザー画面" src="/client" className="w-full h-full" />
       </SmartPhone>
       <div className="w-[400px]">
         <h1 className="text-lg font-semibold mb-2">Chair Simulator</h1>
