fae84d2f33e3bfec5952069d9f5f75cbbb2e0f7b
Kazuhiro Seo <kazuhiro.seo@linecorp.com>
2024-11-27 17:25:13 +0900

    client validationを追加

--- frontend/app/routes/client_.register/route.tsx
+++ frontend/app/routes/client_.register/route.tsx
@@ -1,10 +1,17 @@
 import type { MetaFunction } from "@remix-run/node";
-import { ClientActionFunctionArgs, Form, redirect } from "@remix-run/react";
+import {
+  ClientActionFunctionArgs,
+  Form,
+  json,
+  redirect,
+  useActionData,
+} from "@remix-run/react";
 import { fetchAppPostUsers } from "~/apiClient/apiComponents";
 import { Button } from "~/components/primitives/button/button";
 import { DateInput } from "~/components/primitives/form/date";
 import { TextInput } from "~/components/primitives/form/text";
 import { FormFrame } from "~/components/primitives/frame/form-frame";
+import { Text } from "~/components/primitives/text/text";
 
 export const meta: MetaFunction = () => {
   return [
@@ -15,6 +22,31 @@ export const meta: MetaFunction = () => {
 
 export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
   const formData = await request.formData();
+
+  // client validation
+  const errors: {
+    username?: string;
+    firstname?: string;
+    lastname?: string;
+  } = {};
+
+  const username = String(formData.get("username"));
+  const firstname = String(formData.get("firstname"));
+  const lastname = String(formData.get("lastname"));
+
+  if (username.length > 30) {
+    errors.username = "30文字以内で入力してください";
+  }
+  if (firstname.length > 30) {
+    errors.firstname = "30文字以内で入力してください";
+  }
+  if (lastname.length > 30) {
+    errors.lastname = "30文字以内で入力してください";
+  }
+  if (Object.keys(errors).length > 0) {
+    return json({ errors });
+  }
+
   await fetchAppPostUsers({
     body: {
       date_of_birth: String(formData.get("date_of_birth")),
@@ -27,6 +59,8 @@ export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
 };
 
 export default function ClientRegister() {
+  const actionData = useActionData<typeof clientAction>();
+
   return (
     <FormFrame>
       <h1 className="text-2xl font-semibold mb-8">ユーザー登録</h1>
@@ -38,13 +72,28 @@ export default function ClientRegister() {
             label="ユーザー名"
             required
           />
+          {actionData?.errors?.username && (
+            <Text variant="danger" className="mt-2">
+              {actionData?.errors?.username}
+            </Text>
+          )}
         </div>
         <div className="flex gap-4">
           <div className="w-full">
             <TextInput id="lastname" name="lastname" label="姓" required />
+            {actionData?.errors?.lastname && (
+              <Text variant="danger" className="mt-2">
+                {actionData?.errors?.lastname}
+              </Text>
+            )}
           </div>
           <div className="w-full">
             <TextInput id="firstname" name="firstname" label="名" required />
+            {actionData?.errors?.firstname && (
+              <Text variant="danger" className="mt-2">
+                {actionData?.errors?.firstname}
+              </Text>
+            )}
           </div>
         </div>
         <div>
