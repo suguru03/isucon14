2b05e823786365a016a4767e5a7e2a7710b13ed5
Masanari Hamada <narirou.github@gmail.com>
2024-11-27 23:36:45 +0900

    fix: min-heightの指摘点修正

--- frontend/app/components/primitives/frame/frame.tsx
+++ frontend/app/components/primitives/frame/frame.tsx
@@ -3,9 +3,7 @@ import type { FC, PropsWithChildren } from "react";
 export const MainFrame: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="md:max-w-screen-md h-full relative ml-auto mr-auto shadow-xl bg-white flex flex-col">
-      <div className="flex flex-col" style={{ minHeight: "calc(100vh)" }}>
-        {children}
-      </div>
+      <div className="flex flex-col min-h-screen">{children}</div>
     </div>
   );
 };
