75af86e1a86efb700e8495376f7541625af5a065
Yiyang Zhan <pon.zhan@gmail.com>
2024-11-28 20:35:38 +0900

    python: use latest Ride object in app_post_ride_evaluation

--- webapp/python/app/app_handlers.py
+++ webapp/python/app/app_handlers.py
@@ -502,6 +502,7 @@ def app_post_ride_evaluation(
             raise HTTPException(
                 status_code=HTTPStatus.NOT_FOUND, detail="ride not found"
             )
+        ride = Ride.model_validate(row)
 
         row = conn.execute(
             text("SELECT * FROM payment_tokens WHERE user_id = :user_id"),
